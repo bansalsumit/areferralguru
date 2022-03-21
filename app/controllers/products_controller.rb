5ebb9153d619b509c616940668729f577bb1097f
Sumit Bansal <sbansal.mait@gmail.com>
2022-03-21 15:01:09 +0530

    test

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,7 @@
 class ProductsController < AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
+    
     render(json: { products: @products })
   end
 end
--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,13 @@
 ShopifyApp.configure do |config|
-  config.application_name = "My Shopify App"
+  config.application_name = "A Referral Guru"
   config.old_secret = ""
-  config.scope = "read_products" # Consult this page for more scope options:
+  config.scope = "read_products, read_orders,
+    read_customers, read_discounts, write_discounts, 
+    read_gift_cards, write_gift_cards, write_customers,
+    read_marketing_events, write_marketing_events,
+    read_price_rules, write_price_rules,
+    read_script_tags, write_script_tags,
+    read_locations" # Consult this page for more scope options:
                                   # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
   config.embedded_app = true
   config.after_authenticate_job = false
@@ -13,8 +19,8 @@ ShopifyApp.configure do |config|
   config.allow_jwt_authentication = true
   config.allow_cookie_authentication = false
 
-  config.api_key = ENV.fetch('SHOPIFY_API_KEY', '').presence
-  config.secret = ENV.fetch('SHOPIFY_API_SECRET', '').presence
+  config.api_key = Rails.application.credentials[:shopify][:api_key].presence
+  config.secret = Rails.application.credentials[:shopify][:api_secret].presence
   if defined? Rails::Server
     raise('Missing SHOPIFY_API_KEY. See https://github.com/Shopify/shopify_app#requirements') unless config.api_key
     raise('Missing SHOPIFY_API_SECRET. See https://github.com/Shopify/shopify_app#requirements') unless config.secret
