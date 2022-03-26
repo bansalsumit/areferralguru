6ae685d9e219612a074f4a88b36a4d34bc996e47
Sumit Bansal <sbansal.mait@gmail.com>
2022-03-26 23:55:42 +0530

    test

--- db/migrate/20220319152816_create_shops.rb
+++ db/migrate/20220319152816_create_shops.rb
@@ -3,6 +3,19 @@ class CreateShops < ActiveRecord::Migration[7.0]
     create_table :shops  do |t|
       t.string :shopify_domain, null: false
       t.string :shopify_token, null: false
+      t.string :name
+      t.string :domain
+      t.string :province
+      t.string :country
+      t.string :phone
+      t.string :source
+      t.string :customer_email
+      t.string :email
+      t.string :shop_owner
+      t.string :plan_name
+      t.string :plan_display_name
+      t.string :myshopify_domain
+
       t.timestamps
     end
 
--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_25_065925) do
     t.integer "discount"
     t.string "program"
     t.bigint "shop_id"
+    t.boolean "all_products_allowed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["shop_id"], name: "index_cash_rewards_on_shop_id"
@@ -108,6 +109,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_25_065925) do
   create_table "shops", force: :cascade do |t|
     t.string "shopify_domain", null: false
     t.string "shopify_token", null: false
+    t.string "name"
+    t.string "domain"
+    t.string "province"
+    t.string "country"
+    t.string "phone"
+    t.string "source"
+    t.string "customer_email"
+    t.string "email"
+    t.string "shop_owner"
+    t.string "plan_name"
+    t.string "plan_display_name"
+    t.string "myshopify_domain"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "access_scopes"
