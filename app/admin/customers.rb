e8c9403d6f64e089d0237dd9c409ec964c2770af
Sumit Bansal <sbansal.mait@gmail.com>
2022-03-30 23:49:44 +0530

    Added sync_customer_from_shopify button in customers.

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,13 +1,25 @@
 ActiveAdmin.register Customer do
   permit_params :shop_id, :first_name, :last_name, :email
+  actions :all
 
   filter :active
+  filter :email
+
+  action_item :only => :index do
+    link_to 'Sync customer from shopify', sync_customer_from_shopify_admin_customers_path
+  end
+
+  collection_action :sync_customer_from_shopify do
+    Shopify::ImportCustomers.new(1)
+    redirect_to admin_customers_path, alert: 'Imported customers.'
+  end
 
   index do
     selectable_column
     actions class: 'col-actions left-aligned-col-actions'
     column :first_name
     column :last_name
+    column :email
     column :active
   end
 
