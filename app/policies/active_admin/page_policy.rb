17f7453ff84934dc00f7666a5c54f207c102d117
Sumit Bansal <sbansal.mait@gmail.com>
2022-04-02 20:57:50 +0530

    Added policies for models and active admin.

---
+++ app/policies/active_admin/page_policy.rb
@@ -0,0 +1,17 @@
+class ActiveAdmin::PagePolicy
+  attr_reader :user, :record
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  def show?
+    case record.name
+    when 'Dashboard'
+      true
+    else
+      true
+    end
+  end
+end
