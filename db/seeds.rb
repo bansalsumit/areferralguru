94e348028a3e597ba00a4075b897d57db58f3f96
Sumit Bansal <sbansal.mait@gmail.com>
2022-04-06 20:05:11 +0530

    Chnaged seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-include Sprig::Helpers
-
-sprig [Role, Shop, User]
-user = User.find_by(email: 'admin@example.com')
-user.add_role :admin
-user = User.find_by(email: 'shop_owner@example.com')
-user.add_role :shop_owner
-user = User.find_by(email: 'shop_employee@example.com')
-user.add_role :shop_employee
+admin = Role.find_or_create_by(name: :admin)
+shop_owner = Role.find_or_create_by(name: :shop_owner)
+shop_employee = Role.find_or_create_by(name: :shop_employee)
+bakery_shop = Shop.find_or_create_by(shopify_token: 'first_token', shopify_domain: 'bakery.shop.com', email: 'bakery_shop@gmail.com')
+shoe_shop = Shop.find_or_create_by(shopify_token: 'second_token',shopify_domain: 'shoe.shop.com',email: 'shoe_shop@gmail.com')
+clothes_shop = Shop.find_or_create_by(shopify_token: 'third_token',shopify_domain: 'clothes.shop.com',email: 'clothes_shop@gmail.com')
+admin_user = User.create(email: 'admin@example.com', password: 'password', shop_id: bakery_shop.id)
+shoe_shop_owner = User.create(email: 'shop_owner@example.com', password: 'password', shop_id: shoe_shop.id)
+shoe_shop_employee = User.create(email: 'shop_employee@example.com', password: 'password', shop_id: shoe_shop.id)
+admin_user.add_role :admin
+shoe_shop_owner.add_role :shop_owner
+shoe_shop_employee.add_role :shop_employee
