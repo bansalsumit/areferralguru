de62bba82d34b1ec07e7942d4574ee14f77e76ce
Sumit Bansal <sbansal.mait@gmail.com>
2022-04-17 18:22:10 +0530

    Added attributes to get data of emails.

--- app/models/email_performa.rb
+++ app/models/email_performa.rb
@@ -26,4 +26,35 @@ class EmailPerforma < ApplicationRecord
 
   belongs_to :shop
   belongs_to :email_template
+
+  def self.define_image_attribute(name)
+    define_method("get_" + name.to_s) do
+      if self.send(name).attached?
+        self.send(name)
+      else
+        self.email_template.send("get_" + name.to_s)
+      end
+    end
+  end
+
+  def self.define_email_attribute(name)
+    define_method("get_" + name.to_s) do
+      if self.send(name).present?
+        self.send(name)
+      else
+        self.email_template.send("get_" + name.to_s)
+      end
+    end
+  end
+
+  define_image_attribute :logo
+  define_image_attribute :banner
+
+  define_email_attribute :closing
+  define_email_attribute :email_body
+  define_email_attribute :name
+  define_email_attribute :recipient
+  define_email_attribute :salutation
+  define_email_attribute :sender
+  define_email_attribute :subject
 end
--- app/models/email_template.rb
+++ app/models/email_template.rb
@@ -16,4 +16,27 @@
 class EmailTemplate < ApplicationRecord
   has_one_attached :logo
   has_one_attached :banner
+
+  def self.define_image_attribute(name)
+    define_method("get_" + name.to_s) do
+      self.send(name)
+    end
+  end
+
+  def self.define_email_attribute(name)
+    define_method("get_" + name.to_s) do
+      self.send(name)
+    end
+  end
+
+  define_image_attribute :logo
+  define_image_attribute :banner
+
+  define_email_attribute :closing
+  define_email_attribute :email_body
+  define_email_attribute :name
+  define_email_attribute :recipient
+  define_email_attribute :salutation
+  define_email_attribute :sender
+  define_email_attribute :subject
 end
