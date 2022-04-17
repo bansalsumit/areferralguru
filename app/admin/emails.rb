e05dddc9167657abb808e100d46b968e4260936d
Sumit Bansal <sbansal.mait@gmail.com>
2022-04-17 12:38:17 +0530

    Added email page in active admin.

--- app/admin/emails.rb
+++ app/admin/emails.rb
@@ -1,5 +1,19 @@
 ActiveAdmin.register_page 'Emails' do
+  content title: 'Emails' do
+    emails.each do |email|
+      render partial: 'email', locals: { email: email }
+    end
+  end
+
   action_item :create_email do
     link_to 'Create Template', new_admin_email_template_path
+  end
+
+  controller do
+    before_action :set_emails
+
+    def set_emails
+      @emails = EmailTemplate.all
+    end
   end
 end
